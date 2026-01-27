1a1f8dac01c21e907fe61166bba7101390c3157a
plam <plam84524@gmail.com>
2026-01-27 14:47:07 +0700

    tmp comment unused code

--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,13 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-
-from polls import views
-
-from .views import QuestionList,  ChoiceViewSet
-
-router = DefaultRouter()
-router.register(r"choices", ChoiceViewSet, basename="choices")
-router.register(r"questions", QuestionList,basename="questions")
-urlpatterns = router.urls
-
-# urlpatterns = [
-#     path("questions/", QuestionList.as_view(), name="question-list"),
+# from django.urls import path
+# from rest_framework.routers import DefaultRouter
 #
-# ]
+# from polls import views
+#
+# from .views import QuestionViewSet,  ChoiceViewSet
+#
+# router = DefaultRouter()
+# router.register(r"choices", ChoiceViewSet, basename="choice")
+# router.register(r"questions", QuestionViewSet, basename="questions")
+# urlpatterns = router.urls
+#
+#
