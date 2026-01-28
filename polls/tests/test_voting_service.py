7a2456cb27dcf08784510feb3f2ffa0dcc9b3270
plam <plam84524@gmail.com>
2026-01-28 08:50:55 +0700

    update permission for apis

--- polls/tests/test_voting_service.py
+++ polls/tests/test_voting_service.py
@@ -1,59 +1,40 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.utils import timezone
 from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.utils import timezone
 
-from polls.models import Question, Choice
-from polls.services.voting import (
-    vote,
-    unvote,
-    ChoiceNotFound,
-    InvalidVoteState,
-)
-class VotingServiceTests(TestCase):
+from polls.models import Choice, Question
 
+User = get_user_model()
+
+
+class ChoiceAuthAPITests(APITestCase):
     def setUp(self):
-        self.question = Question.objects.create(
-            question_text="Best backend framework?",
-            pub_date=timezone.now(),
+        self.user = User.objects.create_user(
+            username="alice",
+            password="secret123"
         )
 
         self.choice = Choice.objects.create(
-            question=self.question,
-            choice_text="Django",
-            votes=0,
+            question=Question.objects.create(
+                question_text="Test?",
+                pub_date=timezone.datetime.now()
+            ),
+            choice_text="Yes",
+            votes=0
         )
 
     def test_vote_requires_authentication(self):
         url = reverse("choice-vote", args=[self.choice.id])
-
         response = self.client.post(url)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    # def test_vote_increments_votes(self):
-    #     updated_choice = vote(self.choice.id)
-    #
-    #     self.assertEqual(updated_choice.votes, 1)
-    #
-    #     self.choice.refresh_from_db()
-    #     self.assertEqual(self.choice.votes, 1)
-    #
-    # def test_unvote_decrements_votes(self):
-    #     vote(self.choice.id)
-    #     vote(self.choice.id)
-    #
-    #     updated_choice = unvote(self.choice.id)
-    #
-    #     self.assertEqual(updated_choice.votes, 1)
-    #
-    # def test_unvote_at_zero_raises_error(self):
-    #     with self.assertRaises(InvalidVoteState):
-    #         unvote(self.choice.id)
-    #
-    #     self.choice.refresh_from_db()
-    #     self.assertEqual(self.choice.votes, 0)
-    #
-    # def test_vote_nonexistent_choice_raises(self):
-    #     with self.assertRaises(ChoiceNotFound):
-    #         vote(9999)
+    def test_vote_with_token_succeeds(self):
+        self.client.force_authenticate(user=self.user)
+
+        url = reverse("choice-vote", args=[self.choice.id])
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
