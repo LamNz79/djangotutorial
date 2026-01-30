6284d80a784982aa541cc149254e5c32370ae691
plam <plam84524@gmail.com>
2026-01-30 11:18:58 +0700

    added new vote models.py, updated admin.py to show choice, vote, question

--- polls/admin.py
+++ polls/admin.py
@@ -1,17 +1,35 @@
-from dataclasses import field
 from django.contrib import admin
 
-from polls.models import Question, Choice
+from polls.models import Question, Choice, Vote
+
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
+
+@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets=[
-        (None,{'fields':['question_text']}),
-        ("Date information",{'fields':['pub_date']})
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ("Date information", {'fields': ['pub_date']})
     ]
-    inlines=[ChoiceInline]
-# Register your models here.
-admin.site.register(Question,QuestionAdmin)
+    inlines = [ChoiceInline]
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("choice_text", "question", "vote_count_display")
+    list_select_related = ("question",)
+    ordering = ("question",)
+
+    def vote_count_display(self, obj):
+        return obj.vote_set.count()
+
+    vote_count_display.short_description = "Votes"
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ("user", "question", "choice", "created_at")
+    readonly_fields = ("user", "question", "choice", "created_at")
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
-from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import models, IntegrityError
 
 
 class Question(models.Model):
@@ -12,7 +14,42 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
+    def vote(self, user):
+        """
+        Cast a vote for this choice by a user.
+        Enforces one-vote-per-user-per-choice.
+        """
+        vote = Vote.objects.create(user=user, question=self.question, choice=self)
+        try:
+            vote.full_clean()
+            vote.save()
+            return True
+        except IntegrityError:
+            return False
+
+    @property
+    def vote_count(self):
+        return self.votes.count()
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "question"],
+                name="unique_vote_per_question"
+            )
+        ]
+
+    def clean(self):
+        if self.choice.question.pk != self.question.pk:
+            raise ValidationError("Choice does not belong to the given question")
