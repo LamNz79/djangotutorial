d082dc69cc97ba081a5baae9d3c1daa79de14751
plam <plam84524@gmail.com>
2026-01-30 11:19:27 +0700

    added new condition for abac.py to check if user has voted

--- polls/permissions/abac.py
+++ polls/permissions/abac.py
@@ -1,8 +1,17 @@
 from django.utils import timezone
 from rest_framework.permissions import BasePermission
 
+from polls.models import Vote, Choice
+
 
 class CanVote(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        question = obj.question
-        return question.pub_date <= timezone.now()
+    def has_object_permission(self, request, view, obj: Choice):
+        choice = obj
+        question = choice.question
+        if question.pub_date > timezone.now():
+            return False
+
+        return not Vote.objects.filter(
+            user=request.user,
+            question=choice.question
+        ).exists()
